3c242fb579c6955f417bb59e1163efbf00163d94
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-03-15 20:10:06 +0100

    fix errors of building

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import ContactItem from './ContactItem';
 import { useSelector } from 'react-redux';
-import {
-  contactsArrSelector,
-  filterSelector,
-  filteredContacts,
-} from 'store/Selector';
-import { getAllContactsThunk } from 'store/operations';
+import { filteredContacts } from 'store/Selector';
 
 const ContactList = () => {
-  // const filter = useSelector(filterSelector);
-  // const contactsArr = useSelector(contactsArrSelector);
-
   const contacts = useSelector(filteredContacts);
 
   return (
